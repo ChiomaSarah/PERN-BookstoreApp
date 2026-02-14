5497c9bd02a015c800589bcdb9f50d2410e1de5b
CHIOMA SARAH OSUJI <78868674+ChiomaSarah@users.noreply.github.com>
2026-02-15 00:46:42 +0100

    remove unused useToken from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import useToken from "../useToken";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
